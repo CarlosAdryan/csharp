8a318f9f45db67c5af2ae595587839892604f69c
Carlos Adryan <carlosadryana@gmail.com>
2025-05-05 12:06:42 -0300

    Data output

---
+++ Data-output/Data-output/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_output {
+    class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int age;
+            double salary;
+            string name;
+            char sex;
+
+            age = 32;
+            salary = 4560.9;
+            name = "Maria Silva";
+            sex = 'F';
+
+            Console.WriteLine(name + ", sex " + sex + ", wins " + salary.ToString("F2", CI) + " and is " + age +  " years old");
+        }
+    }
+}
