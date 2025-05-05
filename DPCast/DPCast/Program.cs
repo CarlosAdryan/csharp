35ccb89169b47b1b3eed208981719e8b494e662c
Carlos Adryan <carlosadryana@gmail.com>
2025-05-05 14:05:17 -0300

    Data processing - casting

---
+++ DPCast/DPCast/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPCast {
+    class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double a;
+            int b;
+            a = 5.0;
+            b = (int)a;
+            Console.WriteLine(b);
+
+        }
+    }
+}
