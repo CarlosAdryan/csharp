512e79cc90d0362987249a9fc879c2530c1642ca
Carlos Adryan <carlosadryana@gmail.com>
2025-05-05 11:40:56 -0300

    Variable declaration and assignment

---
+++ csharp-variables/csharp-variables/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_variables {
+    class Program {
+        static void Main(string[] args) {
+
+            int age;
+            double salary, height;
+            char genere;
+            string name;
+
+            age = 32;
+            salary = 4000.9;
+            height = 1.72;
+            genere = 'F';
+            name = "Maria Silva";
+
+            Console.WriteLine(age);
+            Console.WriteLine(salary);
+            Console.WriteLine(height);
+            Console.WriteLine(genere);
+            Console.WriteLine(name);
+        }
+    }
+}
