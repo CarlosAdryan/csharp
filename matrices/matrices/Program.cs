517c3605d1fa3307d20f4693b105be60004e9dee
Carlos Adryan <carlosadryana@gmail.com>
2025-05-07 10:02:53 -0300

    Logic for basic operations with rectangle (area, perimeter, base)

---
+++ matrices/matrices/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrices {
+    class Program {
+        static void Main(string[] args) {
+
+            int M, N, i, j;
+
+            Console.Write("How many lines will the matrix have? ");
+            M = int.Parse(Console.ReadLine());
+            Console.Write("How many columns will the matrix have? ");
+            N = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[M, N];
+
+            for (i = 0; i < M; i++) {
+                for (j = 0; j < N; j++) {
+                    Console.Write("Element [" + i + "," + j + "]: ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("TYPED MATRIX");
+            for (i = 0; i < M; i++) {
+                for (j = 0; j < N; j++) {
+                    Console.Write(mat[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ problems/rectangular/rectangular/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rectangular {
+    class Program {
+        static void Main(string[] args) {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double b, h;
+
+
+            Console.Write("Rectangle base: ");
+            b = double.Parse(Console.ReadLine(), CI);
+            Console.Write("rectangular height: ");
+            h = double.Parse(Console.ReadLine(), CI);
+
+            double area = b * h;
+            double perimeter = 2 * (b + h);
+            double diagonal = Math.Sqrt(b * b + h * h);
+
+            Console.WriteLine("AREA = " + area.ToString("F4", CI));
+            Console.WriteLine("PERIMETER = " + perimeter.ToString("F4", CI));
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4", CI));
+
+        }
+    }
+}
