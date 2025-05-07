c9d2b77fd2b3380e64d637c7f97b2fe7bf961363
Carlos Adryan <carlosadryana@gmail.com>
2025-05-07 13:00:35 -0300

    Algorithm to return the smallest value between three inputs

---
+++ problems/less-than-three/less-than-three/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace less_than_three {
+    class Program {
+        static void Main(string[] args) {
+
+            int a, b, c; 
+
+            Console.Write("First value: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Second value: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("third value: ");
+            c = int.Parse(Console.ReadLine());
+
+            if (a < c && a < b) {
+                Console.WriteLine("MINOR = " + a);
+            } else if (b < c){
+                Console.WriteLine("MINOR = " + b);
+            } else {
+                Console.WriteLine("MINOR = " + c);
+            }
+
+        }
+    }
+}
